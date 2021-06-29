e143fdb206885e9f7e1953afa779901c29b99f43
= <=>
2021-06-29 15:12:10 -0300

    brincando com o POSTMAN

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
     res.send("<h1>Minha lista de tarefas :) 7789 !!</h1>")
 })
 
+app.get("/json", (req,res)=>{
+    res.json({title: "Tarefa X", done: true})
+})
+
+app.get("/barrichelo", (req, res)=>{
+    res.status(200).json({
+        name: "Rubens Barrichelo",
+        nacionality: "Brazil",
+        team: "Ferrari"
+    })
+})
+
 app.listen(3000, () => {
     console.log("Servidor iniciado com sucesso !!")
 })
