effbbbdb5531e2ac3babcb96cd6c5b81a5c188d6
= <=>
2021-06-29 15:32:42 -0300

    Criando uma rota chamada checklist

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const express = require('express')
-
+const checklistRouter = require("./src/routes/checklist")
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("<h1>Minha lista de tarefas :) 7789 !!</h1>")
-})
+app.use(express.json())
 
-app.get("/json", (req,res)=>{
-    res.json({title: "Tarefa X", done: true})
-})
-
-app.get("/barrichelo", (req, res)=>{
-    res.status(200).json({
-        name: "Rubens Barrichelo",
-        nacionality: "Brazil",
-        team: "Ferrari"
-    })
-})
+app.use(checklistRouter)
 
 app.listen(3000, () => {
     console.log("Servidor iniciado com sucesso !!")
