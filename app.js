b21167d8ac81dad80023c42365d1d0eb0c1343e8
= <=>
2021-06-29 15:34:16 -0300

    deixando a rota mais legível

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(checklistRouter)
+app.use('/checklists',checklistRouter)
 
 app.listen(3000, () => {
     console.log("Servidor iniciado com sucesso !!")
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/checklists", (req, res) =>{
+router.get("/", (req, res) =>{
     console.log("Olá")
     res.send()
 })
