08646bca1d3308463b91ca5f2b5b3f59b4715c5b
= <=>
2021-06-30 15:35:35 -0300

    incluindo a relacao de task com checklist

--- src/models/checklist.js
+++ src/models/checklist.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 
 const checklistSchema = mongoose.Schema({
-    name: {type: String, required: true}
-
+    name: {type: String, required: true},
+    tasks:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task'
+    }]
 })
 
 module.exports = mongoose.model('Checklist', checklistSchema)
