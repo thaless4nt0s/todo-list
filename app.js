9982a19e2b84c95bf6c8e552ea4ddf6bec5ffadf
= <=>
2021-07-01 11:01:58 -0300

    configurando os arquivos para as views

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express')
 const path = require('path')
 
 const checklistRouter = require("./src/routes/checklist")
+const rootRouter = require("./src/routes/index")
 require("./config/database")
 
 const app = express()
 app.use(express.json())
 
-app.set("views", path.join(__dirname,'src/ views'))
-app.set("view engine", 'ejs')
+app.set("views", path.join(__dirname,'src/views'))//Diretório onde serão armazenados as views
+app.set("view engine", 'ejs')//qual tipo de arquivo as views terao
 
+app.use('/', rootRouter)
 app.use('/checklists',checklistRouter)
 
 app.listen(3000, () => {
