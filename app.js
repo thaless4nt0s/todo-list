592b379c225d6df28f1bdad6d1ebfd156aadfbfe
= <=>
2021-07-01 10:50:23 -0300

    inserindo o ejs no projeto

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
+
 const checklistRouter = require("./src/routes/checklist")
 require("./config/database")
 
 const app = express()
-
 app.use(express.json())
+
+app.set("views", path.join(__dirname,'src/ views'))
+app.set("view engine", 'ejs')
 
 app.use('/checklists',checklistRouter)
 
