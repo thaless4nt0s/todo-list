9c9b37608123e5f405976b0501c63d292c3c1196
= <=>
2021-07-01 15:22:13 -0300

    visualizando os dados do banco de dados via EJS

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,10 +4,10 @@ const Checklist = require("../models/checklist")
 
 router.get("/", async (req, res) => {
     try {
-        let checklist = await Checklist.find({})
-        res.status(200).json(checklist)
+        let checklists = await Checklist.find({})
+        res.status(200).render("checklists/index",{checklists: checklists})
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).render("pages/error",{error: "Erro ao exibir as listas"})
     }
 })
 
@@ -25,9 +25,9 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     try {
         let checklist = await Checklist.findById(req.params.id)
-        res.status(200).json(checklist)
+        res.status(200).render("checklists/show",{checklist: checklist})
     } catch (error) {
-        res.status(222).json(error)
+        res.status(222).render("pages/error", {error: error})
     }
 })
 
