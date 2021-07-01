9357f09c64a2b84590de4f8ea83b6fec401273cc
= <=>
2021-07-01 09:36:53 -0300

    inserindo operações com o banco de dados

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,28 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const Checklist = require("../models/checklist")
 
-router.get("/", (req, res) =>{
-    console.log("Olá")
-    res.send()
+router.get("/", async (req, res) => {
+    try {
+        let checklist = await Checklist.find({})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 
-router.post("/", (req,res) =>{
-    console.log(req.body)
-    res.status(200).send(req.body)
+router.post("/", async (req, res) => {
+    let { name } = req.body
+    try {
+        let checklist = await Checklist.create({name})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(222).json(error)
+    }
 })
 
-router.get("/:id", (req,res) =>{
-    console.log(req.params)
-    res.status(200).send(`ID: ${req.params.id}`)
+router.get("/:id", async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(222).json(error)
+    }
 })
 
-router.put("/:id", (req,res) =>{
+router.put("/:id", (req, res) => {
     console.log(req.params.id)
     res.status(200).send(`PUT ID: ${req.params.id}`)
 })
 
-router.delete("/:id", (req,res) =>{
+router.delete("/:id", (req, res) => {
     console.log(req.params.id)
     res.status(200).send(`DELETE ID: ${req.params.id}`)
 })
