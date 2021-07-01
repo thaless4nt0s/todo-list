802adecad6cad213b20e80d7efa9af542c3c925a
= <=>
2021-07-01 10:11:28 -0300

    operações de banco de dados com mongoDB

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -31,14 +31,23 @@ router.get("/:id", async (req, res) => {
     }
 })
 
-router.put("/:id", (req, res) => {
-    console.log(req.params.id)
-    res.status(200).send(`PUT ID: ${req.params.id}`)
+router.put("/:id", async (req, res) => {
+    try {
+        let {name} = req.body
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
-router.delete("/:id", (req, res) => {
-    console.log(req.params.id)
-    res.status(200).send(`DELETE ID: ${req.params.id}`)
+router.delete("/:id", async (req, res) => {
+    try{
+        let checklist = await Checklist.findByIdAndDelete(req.params.id)
+        res.status(200).json(checklist)
+    }catch(error){
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router
